767eadab2f67abdeb00f3acc4ec4b821dd3aa4eb
conradmutai <conrad.mutai@gmail.com>
2026-07-28 14:03:23 +0300

    Detection File Complete

--- src/detection.py
+++ src/detection.py
@@ -5,28 +5,39 @@ from pathlib import Path
 # creating a path to the config file to use to initialize the model
 CONFIG_PATH = Path(__file__).resolve().parent.parent / "config" / "config.yaml"
 
+# creating a video path
+VIDEO_PATH = Path(__file__).resolve().parent.parent / "data" / "soccernet" / "england_epl" / "2014-2015" / "2015-02-21 - 18-00 Chelsea 1 - 1 Burnley" / "1_720p.mkv"
+
+# creating a weight path
+weights_path = Path(__file__).resolve().parent / "runs" / "detect" / "train-7" / "weights" / "best.pt"
+
 # # loading YOLO
-# model = YOLO("yolov9m.pt")   # YOLO v9 medium
+# model = YOLO("yolov9s.pt")   # YOLO v9 tiny (pre-trained model)
 #
 # # training the model
 # if __name__ == '__main__':
-#     results = model.train(data=str(CONFIG_PATH))
-
-# loading custom weights over random weights for applying the model
-# model = YOLO('runs/detect/train/weights/best.pt')
+#     results = model.train(
+#         data=str(CONFIG_PATH),
+#         epochs=100,
+#         imgsz=640,
+#         batch=16,
+#         optimizer="AdamW",
+#         device="mps",
+#         name="train-7")
 
 
 def train():
-    model = YOLO('runs/detect/train-4/weights/best.pt')  # loading the best weights into the model for training
+    # loading custom weights over random weights for applying the model
+    model = YOLO(str(weights_path))  # loading the best weights into the model for training
 
-    video_path = r""
+    video_path = str(VIDEO_PATH)
     cap = cv2.VideoCapture(video_path)  # capture of the video
 
     while cap.isOpened():
         success, frame = cap.read()
 
         if success:
-            results = model.track(frame, persist=True, show=True, tracker='botsort.yaml')
+            results = model.track(frame, persist=True, show=False, tracker='botsort.yaml')
             annotated_frame = results[0].plot()  # plots an annotated frame for the tracker
             cv2.imshow("YOLOv9 Tracking", annotated_frame)
 

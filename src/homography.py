a2498d3fdc4750d0bf602c616b60cb1a064dd08a
conradmutai <conrad.mutai@gmail.com>
2026-08-01 14:46:39 +0300

    homography.py finished

--- src/homography.py
+++ src/homography.py
@@ -1,5 +1,15 @@
+import json
+from pathlib import Path
+
 import cv2
 import numpy as np
+
+from sports.configs.soccer import SoccerPitchConfiguration
+
+PITCH_CONFIG = SoccerPitchConfiguration()
+
+# Tracking Path for testing
+KEYPOINT_OUTPUT_PATH = Path(__file__).resolve().parent.parent / "data" / "keypoint_output" / "2015-02-21_chelsea_burnley.jsonl"
 
 
 class ViewTransformer:
@@ -30,3 +40,32 @@ class ViewTransformer:
             np.float32)  # reshapes the points into an applicable form for perspective transform
         points = cv2.perspectiveTransform(points, self.m)  # applies perspective transform to points
         return points.reshape(-1, 2).astype(np.float32)
+
+
+def build_transformer_for_frame(keypoint_output_path):
+    transformers = {}
+
+    # reads through each line of the keypoint path
+    with open(str(keypoint_output_path), "r") as f:
+        for line in f:
+            clean_line = line.strip()
+
+            # skips over if there is an existing issue
+            if not clean_line:
+                continue
+
+            record = json.loads(clean_line)
+            frame = record["frame"]
+            indices = record["indices"]
+            filtered_xy = record["filtered_xy"]
+
+            if len(indices) < 4:
+                continue  # not enough points for a reliable homography
+
+            # gets the source from the filtered_xy and the target coordinates from the pitch_config
+            source = np.array(filtered_xy, dtype=np.float32)
+            target = np.array(PITCH_CONFIG.vertices, dtype=np.float32)[indices]
+
+            transformers[frame] = ViewTransformer(source, target)
+
+    return transformers
